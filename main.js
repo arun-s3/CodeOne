05b63c13c1383419982f9a8b112aeb46a4135b39
arun-s3 <arunsudhakaran01@gmail.com>
2026-01-05 15:45:55 +0530

    Made these sections responsive for all views-- Header, Hero section, Login form and Float features section

--- main.js
+++ main.js
@@ -144,3 +144,22 @@ const floatoObserver = new IntersectionObserver(
 
 floatSections.forEach(section => floatoObserver.observe(section));
 
+
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menuContainer');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('active')
+});
+
+let resizeTimer;
+window.addEventListener('resize', () => {
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        if (window.innerWidth > 900) {
+            menu.classList.remove('active')
+        }
+    }, 75)
+});
+
+
